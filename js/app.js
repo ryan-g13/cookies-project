a5e690dfeeb7711d336162b6a2c6e3e713610429
Ryan Groesch <ryan.groesch@gmail.com>
2018-02-14 12:54:03 -0800

    Linter error fix

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
 //Cookie project Version 0.0
 
 //Array for days of the week
-var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 var hours = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
 var hoursLong = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm ', '8 pm'];
 
